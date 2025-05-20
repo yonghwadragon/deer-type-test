9eb76889d791d52c95e8d41f4e055fb3f4dfec21
yonghwadragon <yongyonghwa@naver.com>
2025-05-20 15:21:26 +0900

    GA태그삽입

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,18 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon_antlers_pair.ico" />
+                {/* GA4 삽입 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QENJW6RRM8"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QENJW6RRM8');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
